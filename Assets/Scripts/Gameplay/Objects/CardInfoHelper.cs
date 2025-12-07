7c11c2e129457695443591cc7a29d6b4e226b75f
Henry Hu <huhenry50@gmail.com>
2025-12-06 23:58:16 -0500

    Fix index bug

--- Assets/Scripts/Gameplay/Objects/CardInfoHelper.cs
+++ Assets/Scripts/Gameplay/Objects/CardInfoHelper.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 
 public static class CardInfoHelper<T>
 {
     public static List<T> OrderDataByID(ulong myId, T[] data, ulong[] dataIDIndices, ulong[] order)
     {
+        List<ulong> idList = dataIDIndices.ToList();
         Dictionary<ulong, T> map = new();
         for (int i = 0; i < data.Length; i++)
         {
@@ -18,11 +20,13 @@ public static class CardInfoHelper<T>
         // Client must figure out turn order relative to itself,
         //    server can also do this for each client though
         List<T> orderedData = new();
-        foreach (ulong clientId in order)
+        for (int i = 0; i < order.Length; i++)
         {
+            ulong clientId = order[i];
             if (myId != clientId)
             {
-                orderedData.Add(data[clientId]);
+                int index = idList.FindIndex(id => id == clientId);
+                orderedData.Add(data[index]);
             }
         }
 
