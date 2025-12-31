fc3a4b915c70ef8780e2151369b491afbdbd065b
Henry Hu <huhenry50@gmail.com>
2025-12-30 21:25:18 -0500

    Move authenticate logic from UI input into separate file

---
+++ Assets/Scripts/AuthenticationManager.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Threading.Tasks;
+using Steamworks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AuthenticationManager : MonoBehaviour
+{
+    public static AuthenticationManager Instance;
+
+    [Header("Firing Events")]
+    [SerializeField] private StringEventChannelSO OnUpdatePlayerDisplayName;
+
+    private void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(Instance.gameObject);
+        }
+
+        Instance = this;
+    }
+
+    public async void Authenticate(string playerName)
+    {
+        try
+        {
+            InitializationOptions initializationOptions = new();
+            initializationOptions.SetProfile(playerName);
+
+            await UnityServices.InitializeAsync(initializationOptions);
+
+            AuthenticationService.Instance.SignedIn += () => {
+#if UNITY_EDITOR
+                Debug.Log("Signed in! " + AuthenticationService.Instance.PlayerId);
+#endif
+                OnUpdatePlayerDisplayName.RaiseEvent(playerName);
+                SceneTransitionHandler.Instance.SetSceneState(SceneStates.MainMenu);
+                SceneManager.LoadScene(SceneTransitionHandler.k_MainMenuScene);
+            };
+
+            string steamTicket = GetSteamTicket(playerName);
+            if (!string.IsNullOrEmpty(steamTicket))
+            {
+                await AuthenticationService.Instance.SignInWithSteamAsync(steamTicket, SteamUser.GetSteamID().ToString());
+            }
+            else
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+        }
+        catch (Exception e)
+        {
+#if UNITY_EDITOR
+            Debug.Log(e.Message);
+#endif
+        }
+    }
+
+    public string GetSteamTicket(string playerName)
+    {
+        try
+        {
+            byte[] ticketBuffer = new byte[1024];
+
+            SteamNetworkingIdentity identity = new();
+            identity.SetSteamID(SteamUser.GetSteamID());
+
+            HAuthTicket authTicket = SteamUser.GetAuthSessionTicket(
+                ticketBuffer,
+                ticketBuffer.Length,
+                out uint ticketSize,
+                ref identity
+            );
+
+            string steamTicket = BitConverter
+                .ToString(ticketBuffer, 0, (int)ticketSize)
+                .Replace("-", string.Empty);
+
+            return steamTicket;
+        }
+        catch (Exception e)
+        {
+#if UNITY_EDITOR
+            Debug.LogError($"Steam auth failed: {e}");
+#endif
+            return null;
+        }
+    }
+
+    private async Task LinkWithSteamAsync(string ticket, string identity)
+    {
+        try
+        {
+            await AuthenticationService.Instance.LinkWithSteamAsync(ticket, identity);
+            Debug.Log("Link is successful.");
+        }
+        catch (AuthenticationException ex) when (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+        {
+            // Prompt the player with an error message.
+            Debug.LogError("This user is already linked with another account. Log in instead.");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("Link failed.");
+            Debug.LogException(ex);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/UI/PlayerProfileNameUI.cs
+++ Assets/Scripts/Gameplay/UI/PlayerProfileNameUI.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Text.RegularExpressions;
 using TMPro;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 [DefaultExecutionOrder(1000)]
@@ -18,41 +14,7 @@ public class PlayerProfileNameUI : MonoBehaviour
     {
         m_confirmButton.onClick.AddListener(() =>
         {
-            Authenticate(s_noWhiteSpace.Replace(m_inputField.text, ""));
+            AuthenticationManager.Instance.Authenticate(s_noWhiteSpace.Replace(m_inputField.text, ""));
         });
-    }
-
-    public async void Authenticate(string playerName)
-    {
-        try
-        {
-            InitializationOptions initializationOptions = new InitializationOptions();
-            initializationOptions.SetProfile(playerName);
-
-            await UnityServices.InitializeAsync(initializationOptions);
-
-            AuthenticationService.Instance.SignedIn += () => {
-#if UNITY_EDITOR
-                Debug.Log("Signed in! " + AuthenticationService.Instance.PlayerId);
-#endif
-                GameManager.Instance.SetLocalPlayerId(AuthenticationService.Instance.PlayerId);
-                GameManager.Instance.SetLocalPlayerName(playerName);
-                SceneTransitionHandler.Instance.SetSceneState(SceneStates.MainMenu);
-                SceneManager.LoadScene(SceneTransitionHandler.k_MainMenuScene);
-            };
-
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
-        catch (Exception e)
-        {
-#if UNITY_EDITOR
-            Debug.Log(e.Message);
-#endif
-        }
-    }
-
-    private void Hide()
-    {
-        gameObject.SetActive(false);
     }
 }
